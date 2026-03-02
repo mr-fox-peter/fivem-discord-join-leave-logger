d8e2a992d3b2670b730742519d63471c1f568ffa
MR FOX 🦊 <sripranee8@gmail.com>
2026-03-02 22:50:19 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.WebhookURL = "YOUR_WEBHOOK"
+Config.WebhookName = "MR FOX's LOG" --Webhook's name 
+Config.WebhookAvatar = "ULR_for_webhook's_avatar" -- URL to an image
+Config.AuthorName = "" --leave it blank or enter your server's name  
+
+-- Colors in Decimal (Green for Join, Red for Leave)
+Config.JoinColor = 3066993 
+Config.LeaveColor = 15158332
